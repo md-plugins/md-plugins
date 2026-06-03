7d8f1bee2deab53597bda978bba3c417ee1af22d
Jeff <galbraith64@gmail.com>
2026-06-03 07:06:46 -0600

    chore: update Quasar app-vite to beta.38

--- packages/qPress/src/install.ts
+++ packages/qPress/src/install.ts
@@ -9,7 +9,7 @@ import { existsSync } from 'node:fs'
 
 export default defineInstallScript(async (api) => {
   api.compatibleWith('quasar', '^2.0.0')
-  api.compatibleWith('@quasar/app-vite', '>=3.0.0-beta.36')
+  api.compatibleWith('@quasar/app-vite', '>=3.0.0-beta.38')
 
   // project must have pinia installed
   if ((await api.getStorePackageName()) !== 'pinia') {
