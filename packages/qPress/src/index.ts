23ebce2ed471fb7f88365189ba5de685dac410ad
Jeff <galbraith64@gmail.com>
2026-06-08 10:07:25 -0600

    chore: update app-vite beta.42

--- packages/qPress/src/index.ts
+++ packages/qPress/src/index.ts
@@ -88,7 +88,7 @@ function extendTypeScriptConfig(typescriptConfig: QuasarTypescriptConfig): void 
 
 export default defineIndexScript((api) => {
   api.compatibleWith('quasar', '^2.0.0')
-  api.compatibleWith('@quasar/app-vite', '>=3.0.0-beta.40')
+  api.compatibleWith('@quasar/app-vite', '>=3.0.0-beta.42')
 
   // here we extend /quasar.config, so we can add some Vite/Vue stuff
   api.extendQuasarConf(async (config) => {
